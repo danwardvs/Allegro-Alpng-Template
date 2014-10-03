647ee4cef0b41acd90b4039e29589f4aa2364db7
danwardvs <danwardvs@adsgames.net>
2014-10-03 18:21:11 -0400

    Added FPS and randnum gen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,64 @@
 #include<allegro.h>
 #include<alpng.h>
+#include<time.h>
 
 BITMAP* buffer;
 
-void update(){
-    draw_sprite(screen,buffer,0,0);
+bool close_button_pressed;
 
+// FPS System
+volatile int ticks = 0;
+const int updates_per_second = 60;
+volatile int game_time = 0;
+
+int fps;
+int frames_done;
+int old_time;
+
+void ticker(){
+  ticks++;
+}
+END_OF_FUNCTION(ticker)
+
+void game_time_ticker(){
+  game_time++;
+}
+END_OF_FUNCTION(ticker)
+
+void close_button_handler(void){
+  close_button_pressed = TRUE;
+}
+END_OF_FUNCTION(close_button_handler)
+
+// Random number generator. Use int random(highest,lowest);
+int random(int newLowest, int newHighest)
+{
+  int lowest = newLowest, highest = newHighest;
+  int range = (highest - lowest) + 1;
+  int randomNumber = lowest+int(range*rand()/(RAND_MAX + 1.0));
+  return randomNumber;
+}
+
+
+//A function to streamline error reporting in file loading
+void abort_on_error(const char *message){
+	 set_window_title("Error!");
+	 if (screen != NULL){
+	    set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
+	 }
+	 allegro_message("%s.\n %s\n", message, allegro_error);
+	 exit(-1);
+}
+
+void update(){
+
+
+
+}
+
+void draw(){
+
+    draw_sprite(screen,buffer,0,0);
 }
 
 
@@ -14,9 +67,27 @@ void update(){
 
 
 
-
 void setup(){
-    buffer=create_bitmap(800,600);
+    buffer=create_bitmap(1024,768);
+
+
+    srand(time(NULL));
+
+     // Setup for FPS system
+    LOCK_VARIABLE(ticks);
+    LOCK_FUNCTION(ticker);
+    install_int_ex(ticker, BPS_TO_TIMER(updates_per_second));
+
+    LOCK_VARIABLE(game_time);
+    LOCK_FUNCTION(game_time_ticker);
+    install_int_ex(game_time_ticker, BPS_TO_TIMER(10));
+
+    // Close button
+    LOCK_FUNCTION(close_button_handler);
+    set_close_button_callback(close_button_handler);
+
+   // if (!(bmp = load_bitmap("bmp.png", NULL)))
+   //   abort_on_error("Cannot find image bmp.png\nPlease check your files and try again");
 }
 
 
@@ -34,18 +105,37 @@ int main(){
   set_color_depth(32);
 
 
-  set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800,600, 0, 0);
+  set_gfx_mode(GFX_AUTODETECT_WINDOWED,1024,768, 0, 0);
   install_sound(DIGI_AUTODETECT,MIDI_AUTODETECT,".");
 
 
 
-  set_window_title("Window title here!");
+  set_window_title("Sci-Fi game!");
   setup();
 
 
-    while(!key[KEY_ESC]){
+      while(!key[KEY_ESC] && !close_button_pressed){
+        while(ticks == 0){
+            rest(1);
+        }
+    while(ticks > 0){
+        int old_ticks = ticks;
+
         update();
-  	}
+
+        ticks--;
+        if(old_ticks <= ticks){
+            break;
+        }
+    }
+        if(game_time - old_time >= 10){
+            fps = frames_done;
+            frames_done = 0;
+            old_time = game_time;
+        }
+        draw();
+    }
+
 
 	return 0;
 }
